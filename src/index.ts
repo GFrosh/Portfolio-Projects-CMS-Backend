c0d1db11a98ffa3b8724728a64d529f17cd87797
SPARTAN <gigi.onyegbula.2007@gmail.com>
2026-04-08 02:34:44 +0100

    fix: used anonymous setup function for cors config

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,23 @@ import logger from './utils/logger.js';
 
 const app = express();
 const PORT = parseInt(envs.PORT, 10) || 3000;
-const whitelist = ["http://localhost:5173", envs.CLIENT_URL];
+const whitelist = ["http://localhost:5173", envs.CLIENT_URL].filter(Boolean);
 const corsOptions = {
-  origin: whitelist,
+  origin: function (origin: any, callback: (err: Error | null, allow?: boolean) => void) {
+    if (!origin || whitelist.includes(origin)) {
+      	callback(null, true);
+    } else {
+    	callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true, // only if using cookies/auth headers needing credentials
+  credentials: true,
 };
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options("/{*any}", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 // ROUTES
 app.use("/api/auth", signupRoutes);
