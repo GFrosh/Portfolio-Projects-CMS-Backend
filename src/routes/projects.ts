f2dc0f38d08ec8feb0dcb5ad5c5b9bf6be476415
GFrosh <gigi.onyegbula.2007@gmail.com>
2026-05-07 11:00:55 +0100

    feat: added `getProject` controller

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -3,10 +3,12 @@ import getAllProjects from "../controllers/getProjects.js";
 import addProject from "../controllers/addProjects.js";
 import updateProject from "../controllers/updateProject.js";
 import deleteProject from "../controllers/deleteProject.js";
+import getProject from "../controllers/getProject.js";
 
 const router = express.Router();
 
 router.get("/", getAllProjects);
+router.get("/view/:id", getProject);
 router.post("/new", addProject);
 router.put("/edit/:id", updateProject);
 router.delete("/delete/:id", deleteProject);
